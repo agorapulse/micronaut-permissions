86d44b74d0437ff156c0bcdc61858c7954297895
musketyr <vladimir@orany.cz>
2022-05-09 10:19:50 +0200

    permissions annotations are now inherited

--- libs/micronaut-permissions/src/main/groovy/com/agorapulse/permissions/GrantsPermission.java
+++ libs/micronaut-permissions/src/main/groovy/com/agorapulse/permissions/GrantsPermission.java
@@ -20,10 +20,7 @@ package com.agorapulse.permissions;
 import io.micronaut.aop.Around;
 import io.micronaut.context.annotation.Type;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
@@ -32,6 +29,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * within a method body.
  */
 @Around
+@Inherited
 @Documented
 @Retention(RUNTIME)
 @Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
